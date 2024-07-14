f7814ab43c498fa0c5c63efac817fd178a5f5cb0
Frida Alexandra Bogacheva <agirre_miron@mail.ru>
2024-07-14 18:00:06 +0300

    first commit

---
+++ m2m/articles/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40, verbose_name="тег")
+
+    class Meta:
+        verbose_name = "раздел"
+        verbose_name_plural = "разделы"
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name="Название")
+    text = models.TextField(verbose_name="Текст")
+    published_at = models.DateTimeField(verbose_name="Дата публикации")
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        verbose_name="Изображение",
+    )
+    scope = models.ManyToManyField(Tag, through="Scope")
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="scopes"
+    )
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BinaryField(default=False)
+
+    def __str__(self):
+        return self.article.title + " " + self.tag.name
+
+    class Meta:
+        verbose_name = "связь"
+        verbose_name_plural = "связи"
+        ordering = ["-is_main", "tag__name"]
